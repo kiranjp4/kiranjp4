ff3c577b9c0d3b0eb1db481122a45cd9f8375e9a
kiranjp4 <kiranjp92@gmail.com>
2022-12-25 23:41:05 +0530

    Added new programs

--- StringPrograms.c
+++ StringPrograms.c
@@ -1,16 +1,38 @@
 #include <stdio.h>
 #include <string.h>
 
-int main()
+int main () 
 {
-    /*To get input from the User and then Prints*/
-    
-    printf("Enter any string :");
-    char InputString[100];
-    scanf("%[^\n]%*c",InputString);
-    
-    printf("Enter string is : %s",InputString);
-    
-    return 0;
-    
+    /*Local Variable*/
+  	char InputString[100];
+   
+	/*1. To get input from the User and then Prints */ 
+	
+	printf ("Enter any string :");
+
+	scanf ("%[^\n]%*c", InputString);
+	printf ("Entered string is : %s\n", InputString);
+
+ 
+	/*2.  To find the length of the string */ 
+	
+	char StringIndex = 0;
+	while (InputString[StringIndex] != '\0')
+	{
+		StringIndex++;
+	}
+	printf ("Entered Length of the string is =%d\n", StringIndex);
+
+ 
+    /*3. To Seperate the string */ 
+    StringIndex = 0;
+    while(InputString[StringIndex] != '\0')
+    {
+        printf("%c ",InputString[StringIndex]);
+        StringIndex++;
+    }
+
+	return 0;
+
+ 
 }
